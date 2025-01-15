23ff595a6878ca1e340a22a1cf09a56bbcd8a91d
vjai25 <virendra.jai25@gmail.com>
2025-01-15 17:26:23 +0530

    code added for route handler and serer start

--- microservice-demo/index.js
+++ microservice-demo/index.js
@@ -1,1 +1,16 @@
+ const express = require('express');
+ const app = express();
+ const port = 3000;
+
+ // define a simple route
+
+ app.get('/', (req, res) => {
+        res.send('Hello this is simple microservices');
+    });
+
+// start the server
+app.listen(port, () => {
+    console.log(`Microservice running at http://localhost:${port}`)
+
+} );
  
